cc5f3f16d76bf496b1d276b5bfac4cac4fd9208e
hncg <574878649@qq.com>
2017-06-14 23:56:32 +0800

    rm utils format function()

--- src/common/utils.js
+++ src/common/utils.js
@@ -1,19 +1,3 @@
 /**
  * 通用函数
  */
-
-var format = function (string) {
-  var args = arguments
-  var pattern = new RegExp('{([0-' + arguments.length + '])}', 'g')
-  return String(string).replace(pattern, function (match, index) {
-    var currentIndex = parseInt(index)
-    if (currentIndex + 1 > args.length || currentIndex < 0) {
-      throw new Error('参数索引出错')
-    }
-    return args[currentIndex + 1]
-  })
-}
-
-export default {
-  format: format
-}
